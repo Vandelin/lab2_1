f26b99c15a6742bac75ae565bd57432067228ed0
Piotr Rogowski <polishotaku@gmail.com>
2019-03-25 19:59:20 +0100

    Class BinarySearchTest added.

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.bsearch;
+
+import junit.extensions.TestSetup;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinarySearchTest {
+
+    @Before
+    public void setup() {
+    }
+}
