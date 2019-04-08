f1e8698b4f597d8bd248391b6c582a265f618906
Piotr Rogowski <polishotaku@gmail.com>
2019-04-08 22:48:26 +0200

    testy_v1 Added sequence lenght = 1 and sequence found test.

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.bsearch;
 
 import junit.extensions.TestSetup;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,7 +9,12 @@ import static org.junit.Assert.*;
 
 public class BinarySearchTest {
 
-    @Before
-    public void setup() {
+    @Test
+    public void searchTestSeqLenghtOneFound() {
+        int[] seq = {123};
+        SearchResult sr = BinarySearch.search(123, seq);
+        Assert.assertTrue(sr.isFound());
     }
+
+
 }
